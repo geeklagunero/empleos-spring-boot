44fe989dd6d5ac32c6f0b431e309a3bac1f63497
ricardo roman <ricardomon85@gmail.com>
2021-01-04 18:20:02 -0600

    fisrt Commit

---
+++ src/main/java/com/applaguna/empleospring/EmpleospringApplication.java
@@ -0,0 +1,13 @@
+package com.applaguna.empleospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleospringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmpleospringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/applaguna/empleospring/EmpleospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.applaguna.empleospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleospringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
